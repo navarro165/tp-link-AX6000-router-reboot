4930fc98102d0f603e38e380e8c0145d1f1a56c7
Carlos Navarro <navarro165@gmail.com>
2024-01-14 19:26:48 -0600

    commands need to run in bg regardless

--- reboot.sh
+++ reboot.sh
@@ -4,10 +4,9 @@ docker pull python:3.10-slim
 docker pull selenium/standalone-firefox:4.16.1-20231219
 
 docker network create selenium-host
-docker compose up --exit-code-from=python-app
 
-docker compose down --volumes
-docker network rm selenium-host
+(docker compose up --exit-code-from=python-app ; docker compose down --volumes ; docker network rm selenium-host) &
+
 
 
 # reference:
