939f943008ca502ba5cedb0860cb3f45a02b26d9
Carlos Navarro <navarro165@gmail.com>
2024-01-21 13:34:55 -0600

    improve reboot.py by replacing sleeps with explicit waits

--- reboot.py
+++ reboot.py
@@ -1,62 +1,46 @@
 #!/usr/bin/env python3
 
 import os
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 ROUTER_IP = os.environ['ROUTER_REBOOT_IP']
 USERNAME = os.environ['ROUTER_REBOOT_USERNAME']
 PASSWORD = os.environ['ROUTER_REBOOT_PASSWORD']
 
-
-driver = None
-
-
-def run():
-    driver = webdriver.Remote(command_executor='http://localhost:4444', options=webdriver.FirefoxOptions())
+def run(driver):
     driver.get(f'http://{ROUTER_IP}/webpages/index.html')
     driver.set_window_size(1247, 1095)
 
-    time.sleep(3)
-    driver.find_element(By.LINK_TEXT, "TP-Link ID").click()
-
-    time.sleep(3)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "TP-Link ID"))).click()
     driver.switch_to.frame(0)
 
-    driver.find_element(By.CSS_SELECTOR, ".form-group:nth-child(1) .input-group").click()
-    driver.find_element(By.ID, "login_inputUser").send_keys(USERNAME)
+    username_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "login_inputUser")))
+    username_input.click()
+    username_input.send_keys(USERNAME)
 
-    time.sleep(3)
-    driver.find_element(By.CSS_SELECTOR, ".has-feedback").click()
-    driver.find_element(By.ID, "login_inputPassword").send_keys(PASSWORD)
+    password_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "login_inputPassword")))
+    password_input.click()
+    password_input.send_keys(PASSWORD)
     driver.find_element(By.ID, "login_login").click()
 
-    time.sleep(3)
     driver.switch_to.default_content()
-    driver.find_element(By.LINK_TEXT, "Advanced").click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Advanced"))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "System"))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Reboot"))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "reboot-button"))).click()
 
-    time.sleep(5)
-    driver.find_element(By.LINK_TEXT, "System").click()
-
-    time.sleep(5)
-    driver.find_element(By.LINK_TEXT, "Reboot").click()
-
-    time.sleep(5)
-    driver.find_element(By.ID, "reboot-button").click()
-
-    time.sleep(5)
     driver.quit()
 
-
 try:
-    run()
-except Exception:  # try again
-    if driver is not None:
+    driver = webdriver.Remote(command_executor='http://localhost:4444', options=webdriver.FirefoxOptions())
+    run(driver)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    if driver:
         driver.quit()
-    time.sleep(5)
-    run()
 finally:
-    if driver is not None:
+    if driver:
         driver.quit()
